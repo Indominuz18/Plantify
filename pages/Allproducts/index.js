849744ea43f6900f5db7ae6f8c93c00e0845b9a4
Indominuz18 <84493694+Indominuz18@users.noreply.github.com>
2023-03-06 23:25:24 -0800

    staticProps fix

--- pages/Allproducts/index.js
+++ pages/Allproducts/index.js
@@ -9,30 +9,30 @@ export default function Allproducts(props) {
   return <AllProducts products={props.products} />;
 }
 
-export async function getStaticProps() {
-  const client = await MongoClient.connect(url);
-  const db = client.db();
+// export async function getStaticProps() {
+//   const client = await MongoClient.connect(url);
+//   const db = client.db();
 
-  const productCollection = db.collection("products");
+//   const productCollection = db.collection("products");
 
-  const products = await productCollection.find().toArray();
+//   const products = await productCollection.find().toArray();
 
-  // const final_products = JSON.stringify(products).toArray()
+//   // const final_products = JSON.stringify(products).toArray()
 
-  const final_products = products.map((product) => {
-    return {
-      id: product._id.toString(),
-      title: product.title,
-      description: product.description,
-      img: product.img,
-      price: product.price,
-    };
-  });
+//   const final_products = products.map((product) => {
+//     return {
+//       id: product._id.toString(),
+//       title: product.title,
+//       description: product.description,
+//       img: product.img,
+//       price: product.price,
+//     };
+//   });
 
-  console.log(final_products);
+//   console.log(final_products);
 
-  return {
-    props: final_products,
-    revalidate: 1,
-  };
-}
+//   return {
+//     props: final_products,
+//     revalidate: 1,
+//   };
+// }
